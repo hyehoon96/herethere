71feb8b1d49f1b1acc9040f0207ce69aaf18ba81
ajy9844 <maloo.dot@gmail.com>
2022-03-13 02:21:00 +0900

    feat : add database connection termination module

--- database.js
+++ database.js
@@ -16,10 +16,19 @@ module.exports = {
     connect: function(conn) {
         conn.connect(function(err) {
             if (err) {
-                console.error('mysql connection failed: ' + err.stack);
+                console.error('데이터베이스 연결에 실패했습니다: ' + err.stack);
             } else {
-                console.log('mysql is successfully connected! thread_id: ' + conn.threadId);
+                console.log('데이터베이스를 연결했습니다: ' + conn.threadId);
             }
         });
+    },
+    end: function(conn) {
+        conn.end(function(err) {
+            if (err) {
+                console.error('데이터베이스 연결 종료에 실패했습니다: ' + err.stack);
+            } else {
+                console.log('데이터베이스 연결을 종료했습니다: ' + conn.threadId);
+            }
+        })
     }
 }
