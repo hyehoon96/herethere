8748fad1cc12d57e7c9c554edf014a5d9ece88c8
ajy9844 <maloo.dot@gmail.com>
2022-03-20 01:42:17 +0900

    refactor : rename users to user in api url

--- server/app.js
+++ server/app.js
@@ -46,66 +46,6 @@ app.use(session({
     },
 }));
 
-// 회원정보를 가져오는 API
-app.get('/users/:id', (req, res) => {
-    const id = req.params.id;
-    conn.query('SELECT id, password, name, nickname FROM user WHERE id = ? AND is_deleted = "N"', id, (err, row) => {
-        if (err) { console.log(err); }
-        let user = row[0];
-        if (!user) {
-            return res.status(404).json({err: 'Unknown user'});
-        }
-        res.json(user);
-    });
-});
-
-// 회원정보를 삭제하는 API
-// 1. 테이블에서 데이터를 삭제하는 방법
-// 2. 플래그를 사용하여 데이터가 삭제되었다고 표시 -> 이거 적용!
-app.delete('/users/:id', (req, res) => {
-    const id = req.params.id;
-    conn.query('UPDATE user SET is_deleted = "Y", deleted_date = CURRENT_TIMESTAMP WHERE id = ?', id, (err, row) => {
-        if (err) { console.log(err); }
-        return res.status(204).send();
-    });
-});
-
-// 회원정보를 생성하는 API
-// 조건1. 중복이 아닌 아이디
-// 조건2. 생성 규칙을 준수하는 비밀번호
-app.post('/users', (req, res) => {
-    const params = [
-        req.body.id.toLowerCase(), // 소문자로 변환
-        req.body.password,
-        req.body.name,
-        req.body.nickname
-    ];
-    bcrypt.hash(params[1], 10, (err, hash) => {
-        params[1] = hash;
-        conn.query('INSERT INTO user(`id`,`password`,`name`,`nickname`) VALUES (?,?,?,?)', params, (err, row) => {
-            if (err) { console.log(err); }
-            return res.status(201).json({
-                no: row['insertId'],
-                id: params[0],
-                password: params[1],
-                name: params[2],
-                nickname: params[3]
-            });
-        });
-    });
-});
-
-// 아이디 유효성 검사 API
-app.get('/users/checkId/:id', (req, res) => {
-    const id = req.params.id;
-    conn.query('SELECT id FROM user WHERE id = LOWER(?)', id, (err, row) => {
-        if (err) { console.log(err); }
-        return res.status(200).json({
-            result: row[0] ? false : true
-        });
-    })
-});
-
 // page router 연결
 app.use('/', pageRouter);
 
@@ -128,11 +68,69 @@ app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 대기중');
 })
 
+// 회원정보를 가져오는 API
+app.get('/user/:id', (req, res) => {
+    const id = req.params.id;
+    conn.query('SELECT id, password, name, nickname FROM user WHERE id = ? AND is_deleted = "N"', id, (err, row) => {
+        if (err) { console.log(err); }
+        let user = row[0];
+        if (!user) {
+            return res.status(404).json({err: 'Unknown user'});
+        }
+        res.json(user);
+    });
+});
+
+// 회원정보를 삭제하는 API
+// 1. 테이블에서 데이터를 삭제하는 방법
+// 2. 플래그를 사용하여 데이터가 삭제되었다고 표시 -> 적용!
+app.delete('/user/:id', (req, res) => {
+    const id = req.params.id;
+    conn.query('UPDATE user SET is_deleted = "Y", deleted_date = CURRENT_TIMESTAMP WHERE id = ?', id, (err, row) => {
+        if (err) { console.log(err); }
+        return res.status(204).send();
+    });
+});
+
+// 회원정보를 생성하는 API
+// 조건1. 중복이 아닌 아이디
+// 조건2. 생성 규칙을 준수하는 비밀번호
+app.post('/user', (req, res) => {
+    const params = [
+        req.body.id.toLowerCase(), // 소문자로 변환
+        req.body.password,
+        req.body.name,
+        req.body.nickname
+    ];
+    bcrypt.hash(params[1], 10, (err, hash) => {
+        params[1] = hash;
+        conn.query('INSERT INTO user(`id`,`password`,`name`,`nickname`) VALUES (?,?,?,?)', params, (err, row) => {
+            if (err) { console.log(err); }
+            return res.status(201).json({
+                no: row['insertId'],
+                id: params[0],
+                password: params[1],
+                name: params[2],
+                nickname: params[3]
+            });
+        });
+    });
+});
+
+// 아이디 유효성 검사 API
+app.get('/user/checkId/:id', (req, res) => {
+    const id = req.params.id;
+    conn.query('SELECT id FROM user WHERE id = LOWER(?)', id, (err, row) => {
+        if (err) { console.log(err); }
+        return res.status(200).json({
+            result: row[0] ? false : true
+        });
+    })
+});
+
 // todo: 서버 종료 시 데이터베이스 연결이 종료되도록 수정
 process.on('SIGINT', function() {
     console.log('프로세스를 종료합니다');
-    app.close(function() {
-        database.end(conn);
-        process.exit(0);
-    })
+    database.end(conn);
+    process.exit(0);
 })
