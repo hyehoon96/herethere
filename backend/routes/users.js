a21feca19450c034f525134a32ea7f8435b9b9ac
ajy9844 <ajy09844@gmail.com>
2022-04-17 17:45:59 +0900

    feat : edit `create user` API

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,10 +2,13 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const router = express.Router();
 const database = require('../database');
-// 회원정보를 생성하는 API
-// 조건1. 중복이 아닌 아이디
-// 조건2. 생성 규칙을 준수하는 비밀번호
 
+
+/**
+ * 회원정보 생성 API
+ * - 조건1. 중복이 아닌 아이디
+ * - 조건2. 작성규칙을 준수하는 비밀번호
+ */
 router.post('/', (req, res) => {
   conn = database.init();
   console.log(req.body);
@@ -13,18 +16,57 @@ router.post('/', (req, res) => {
     req.body.id.toLowerCase(), // 소문자로 변환
     req.body.password,
     req.body.name,
-    req.body.nickname
+    req.body.nickname,
+    req.body.ageGroup,
+    req.body.gender,
+    req.body.question,
+    req.body.answer
   ];
+
+  // 컬럼값 저장형식 변환
+  // 예. '20대' -> '20-29'
+  let ageGroup = req.body.ageGroup;
+  switch (ageGroup) {
+    case '10대':
+      ageGroup = '10-19';
+      break;
+    case '20대':
+      ageGroup = '20-29';
+      break;
+    case '30대':
+      ageGroup = '30-39';
+      break;
+    case '40대':
+      ageGroup = '40-49';
+      break;
+    case '50대':
+      ageGroup = '50-59';
+      break;
+    case '60대':
+      ageGroup = '60+';
+      break;
+  }
+  // 예. '남성' -> 'M'
+  let gender = req.body.gender;
+  if (gender) {
+    gender = (gender === "남성") ? 'M' : 'W';
+  }
+
   bcrypt.hash(params[1], 10, (err, hash) => {
     params[1] = hash;
-    conn.query('INSERT INTO user(`userid`,`password`,`name`,`nickname`) VALUES (?,?,?,?)', params, (err, row) => {
+    params[4] = ageGroup;
+    params[5] = gender;
+    conn.query('INSERT INTO user(`userid`,`password`,`name`,`nickname`,`age_group`,`gender`,`question`,`answer`) VALUES (?,?,?,?,?,?,?,?)'
+        , params, (err, row) => {
       if (err) { console.log(err); }
       return res.status(201).json({
         // no: row['insertId'],
         userid: params[0],
-        password: params[1],
+        // password: params[1],
         name: params[2],
-        nickname: params[3]
+        nickname: params[3],
+        ageGroup: params[4],
+        gender: params[5]
       });
     });
     database.end(conn);
