e6eefbd64b2c39646debfeb36421bfc593d841d1
ajy9844 <ajy09844@gmail.com>
2022-04-25 04:19:14 +0900

    refactor : rename database config variable

--- backend/database.js
+++ backend/database.js
@@ -3,8 +3,8 @@ const mysql = require('mysql');
 const db_config = {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
     database: process.env.DB_NAME
 }
 
