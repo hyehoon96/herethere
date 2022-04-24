65263ff2e4168fe4702fc5f14b47ffe13d057241
ajy9844 <ajy09844@gmail.com>
2022-04-25 04:32:50 +0900

    feat : add auth router

--- backend/app.js
+++ backend/app.js
@@ -1,66 +1,58 @@
-const express       = require('express');
-const cookieParser  = require('cookie-parser');
-const morgan        = require('morgan');            // 서버 요청에 따라 log 기록용
-const path          = require('path');              // 파일 경로
-const session       = require('express-session');   // 사용자의 데이터를 임시적으로 저장함
-const database      = require('./database.js');
-const webSocket     = require('./socket.js');
+var express = require('express');
+var app = (module.exports = express());
+var session = require("express-session");
+var morgan = require('morgan'); // 클라이언트 요청에 따라 로그 기록
+var path = require('path');     // 파일과 디렉터리 경로를 편리하게 설정
 
-/*
-  Node.js 서버의 설정(환경변수)을 받아오기 위한 dotenv 모듈 불러오기
-  설정이 적용된 후에, 나머지 코드들이 실행되기 위해서 꼭 맨위에 작성해야 한다
-*/
+const database = require('./database.js');
+const webSocket = require('./socket.js');
+
+var indexRouter = require('./routes/index');
+var userRouter = require('./routes/user');
+var roomRouter = require('./routes/room');
+var authRouter = require('./routes/auth');
+
 require('dotenv').config();
-const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.set('port', process.env.PORT || 8080);
 
-// 데이터베이스 연결
-const conn = database.init();
-database.connect(conn);
-
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cookieParser(process.env.COOKIE_SECRET));
+
 const sessionMiddleware = session({
+    key: "session_cookie_name",
+    secret: "session_cookie_secret",
     resave: false,
     saveUninitialized: false,
-    secret: process.env.COOKIE_SECRET,
     cookie: {
-        httpOnly: true,
-        secure: false,
-    },
+        maxAge: 1000 * 60 * 60 // 쿠키 유효기간 60분
+    }
 })
 app.use(sessionMiddleware);
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var roomRouter = require('./routes/room');        
 app.use('/', indexRouter);
-app.use('/api/user', usersRouter);
+app.use('/api/user', userRouter);
 app.use('/api/room', roomRouter);
+app.use('/api/auth', authRouter);
+
 app.use((req, res, next) => {
     const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
     error.status = 404;
     next(error);
 });
 
-app.use((err, req, res, next)=> {
+app.use((err, req, res, next) => {
     res.locals.message = err.message;
     res.locals.error = process.env.NODE_ENV !== 'production' ? err: {};
     res.status(err.status || 500);
     res.render('error');
 });
 
-// port에서 대기
+// 포트에서 대기
 const server = app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 대기중');
 })
 webSocket(server, app, sessionMiddleware);
-
-
-
-module.exports = app;
