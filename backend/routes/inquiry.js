26f40394c44a71aa1e9cee677d953ac1dc8f4571
hyehoon96 <joojup2@naver.com>
2022-05-11 19:22:34 +0900

    feat : add inquiry GET api

--- backend/routes/inquiry.js
+++ backend/routes/inquiry.js
@@ -2,28 +2,39 @@ const express = require('express');
 const router = express.Router();
 const database = require('../database');
 
-
-router.post('/', (req, res) => {
-  if (!req.session.user) {
-    return res.status(401).json({
-        message: '로그인 되지 않은 사용자입니다.'
-    });
-  }
-   
-  conn = database.init();
-  
-  const params = [
-    req.body.title,
-    req.body.text
-  ]
-  conn.query('INSERT INTO inquiry(`title`, `text`) VALUES (?, ?)', params, (err, row) => {
-    if(err) {
-      console.log(err);
-      return res.status(404).send('error');
+router.route('/')
+  .all((req, res, next) => {
+    if (!req.session.user) {
+        return res.status(401).json({
+            message: '로그인 되지 않은 사용자입니다.'
+        });
+    } else {
+        req.conn = database.init();
+        next();
     }
-    return res.status(201).json(row)
   })
-  database.end();
-  
-})
+  .post((req, res) => {
+    const params = [
+      req.body.title,
+      req.body.text
+    ]
+    req.conn.query('INSERT INTO inquiry(`title`, `text`) VALUES (?, ?)', params, (err, row) => {
+      if(err) {
+        console.log(err);
+        return res.status(404).send('error');
+      }
+      return res.status(201).json(row)
+    })
+    database.end();
+  })
+  .get((req, res) => {
+    req.conn.query('SELECT * FROM inquiry', (err, row) => {
+      if(err) {
+        console.log(err);
+        return res.status(404).send('Not found');
+      }
+      return res.status(201).json(row)
+    }) 
+  })
+
 module.exports = router;
