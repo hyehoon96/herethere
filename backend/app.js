a6d967d27cb7fe8ac981a8ef6bf195d7e8e3f6d5
ajy9844 <ajy09844@gmail.com>
2022-05-22 03:27:53 +0900

    build : edit app port

--- backend/app.js
+++ backend/app.js
@@ -98,6 +98,6 @@ app.use((err, req, res, next) => {
 
 // 포트에서 대기
 const server = app.listen(app.get('port'), () => {
-    console.log(app.get('port'), '번 포트에서 대기중');
+    logger.info(app.get('port'), '번 포트에서 대기중');
 })
 webSocket(server, app, sessionMiddleware);
