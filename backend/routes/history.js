58fa7862e5b3b4582d803873af833a7e4ba3f413
hyehoon96 <joojup2@naver.com>
2022-06-19 15:23:44 +0900

    chore : change history router sequence

--- backend/routes/history.js
+++ backend/routes/history.js
@@ -3,23 +3,6 @@ const router = express.Router();
 const database = require('../database');
 const logger = require('../logger');
 
-router.get('/ranking', (req, res) => {
-    req.conn = database.init();
-
-    req.conn.query('SELECT * FROM place WHERE (updated_date BETWEEN DATE_ADD(NOW(),INTERVAL -1 MONTH ) AND NOW()) AND views > 0 ORDER BY views DESC LIMIT 10 ', (err, row) => {
-        if(err) {
-            logger.error(err)
-            return res.status(500).send({message: '서버 오류가 발생했습니다.'});
-        } 
-        if (row) {
-            return res.status(200).json(row);
-        } else {
-            return res.status(404).send({message: '데이터가 충분하지 않아 조회할 수 없습니다.'});
-        }
-    })
-    database.end(req.conn);
-})
-
 router.route('/')
     .all((req, res, next) => {
         if (req.session.key) {
@@ -126,5 +109,22 @@ router.route('/:placeId')
         });
         database.end(req.conn);
     })
+
+router.get('/ranking', (req, res) => {
+    req.conn = database.init();
+
+    req.conn.query('SELECT * FROM place WHERE (updated_date BETWEEN DATE_ADD(NOW(),INTERVAL -1 MONTH ) AND NOW()) AND views > 0 ORDER BY views DESC LIMIT 10 ', (err, row) => {
+        if(err) {
+            logger.error(err)
+            return res.status(500).send({message: '서버 오류가 발생했습니다.'});
+        } 
+        if (row) {
+            return res.status(200).json(row);
+        } else {
+            return res.status(404).send({message: '데이터가 충분하지 않아 조회할 수 없습니다.'});
+        }
+    })
+    database.end(req.conn);
+})
 
 module.exports = router;
